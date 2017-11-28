bbc9a20fc57ea3ef7a3a638976d2a673004a05f7
Minttu Hurme <ammsalme@users.noreply.github.com>
2017-11-28 10:39:44 +0200

    Add classifications and subjects -profile

---
+++ frontend/js/config/merge-profiles/classifications_and_subjects.js
@@ -0,0 +1,58 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* UI for merging MARC records
+*
+* Copyright (C) 2015-2017 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of marc-merge-ui
+*
+* marc-merge-ui program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* oai-pmh-server-backend-module-melinda is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+/*eslint-disable quotes*/
+
+import { preset as MergeValidationPreset } from '../../marc-record-merge-validate-service';
+import { preset as PostMergePreset } from '../../marc-record-merge-postmerge-service';
+import TargetRecord from '../target-record';
+import * as subrecordMergeTypes from '../subrecord-merge-types';
+
+module.exports = {
+  "name": "Luokitukset ja sisällönkuvailu",
+  "description": "Muunnos kopioi luokitus- ja sisällönkuvailukentät lähdetietueesta kohdetietueeseen. Mahdollisia osakohteita ei käsitellä.",
+  "record": {
+    "targetRecord": TargetRecord,
+    "validationRules": MergeValidationPreset.melinda_host,
+    "postMergeFixes": PostMergePreset.defaults,
+    "mergeConfiguration": {
+      "fields": {
+        "05.":    { "action": "copy", "options": {"reduce": { "subfields": ["9"], "condition": "unless", "value": /[LOWTAG]<(KEEP|DROP)>/ } } },
+        "06[01]": { "action": "copy", "options": {"reduce": { "subfields": ["9"], "condition": "unless", "value": /[LOWTAG]<(KEEP|DROP)>/ } } },
+        "07[012]": { "action": "copy", "options": {"reduce": { "subfields": ["9"], "condition": "unless", "value": /[LOWTAG]<(KEEP|DROP)>/ } } },
+        "08[023456]": { "action": "copy", "options": {"reduce": { "subfields": ["9"], "condition": "unless", "value": /[LOWTAG]<(KEEP|DROP)>/ } } },
+        "6..": { "action": "copy", "options": {"reduce": { "subfields": ["9"], "condition": "unless", "value": /[LOWTAG]<(KEEP|DROP)>/ } } },
+      }
+    },
+  },
+  "subrecords": {
+    "mergeType": subrecordMergeTypes.KEEP_BOTH
+  }
+};
+
+/*eslint-enable quotes*/
