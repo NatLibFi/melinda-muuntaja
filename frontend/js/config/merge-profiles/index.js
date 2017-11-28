7ed7436c6e643ec60926372d33f318431b57552e
Minttu Hurme <ammsalme@users.noreply.github.com>
2017-11-28 10:56:33 +0200

    Update index.js

--- frontend/js/config/merge-profiles/index.js
+++ frontend/js/config/merge-profiles/index.js
@@ -1,3 +1,4 @@
 exports.default = require('./default');
+exports.classifications_and_subjects = require('./classifications_and_subjects');
 exports.no_subrecords = require('./no_subrecords');
 exports.shared = require('./shared');
