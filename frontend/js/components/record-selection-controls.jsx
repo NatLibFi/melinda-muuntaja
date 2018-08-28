bd902ebb9f7d0d2d79dd735605c818784e44e58c
harri-h <harri.huhtala@mystes.fi>
2018-08-28 08:51:19 +0300

    formatted record-selection-controls.jsx file

--- frontend/js/components/record-selection-controls.jsx
+++ frontend/js/components/record-selection-controls.jsx
@@ -37,7 +37,6 @@ import { hostRecordActionsEnabled } from '../selectors/merge-status-selector';
 import classNames from 'classnames';
 
 const RECORD_LOADING_DELAY = 500;
-
 export class RecordSelectionControls extends React.Component {
 
   static propTypes = {
@@ -75,13 +74,11 @@ export class RecordSelectionControls extends React.Component {
   componentDidUpdate() {
     // update text fields if they are prefilled.
     window.Materialize && window.Materialize.updateTextFields();
-
     window.$(this.mergeProfileSelect).on('change', (event) => this.handleMergeProfileChange(event.target.value)).material_select();
   }
 
   componentWillUnmount() {
     document.removeEventListener('click', this.closeProfileInfo, false);
-
     if (typeof this.unlisten == 'function') {
       this.unlisten();
     }
@@ -95,7 +92,6 @@ export class RecordSelectionControls extends React.Component {
 
   UNSAFE_componentWillReceiveProps(next) {
     if (next.targetRecordId === this.props.targetRecordId && next.sourceRecordId === this.props.sourceRecordId) return;
-
     if (_.identity(next.targetRecordId) && _.identity(next.sourceRecordId)) {
       this.props.history.push(`/record/${next.sourceRecordId}/to/${next.targetRecordId}`);
     }
@@ -127,7 +123,6 @@ export class RecordSelectionControls extends React.Component {
       else {
         this.props.resetSourceRecord();
       }
-
     }
     if (event.target.id === 'target_record') {
       if (event.target.value.length > 0) {
@@ -142,11 +137,9 @@ export class RecordSelectionControls extends React.Component {
 
   handleSwap() {
     const { controlsEnabled } = this.props;
-
     if (controlsEnabled) {
       this.props.swapRecords();
     }
-
   }
 
   closeProfileInfo = (e) => {
@@ -154,59 +147,65 @@ export class RecordSelectionControls extends React.Component {
       this.setState({
         displayProfileInfo: false
       });
-
       document.removeEventListener('click', this.closeProfileInfo, false);
     }
   }
 
   displayProfileInfo(e) {
     e.preventDefault();
-
     this.setState({
       displayProfileInfo: true
     });
-
     document.addEventListener('click', this.closeProfileInfo);
   }
 
   render() {
-
     const { controlsEnabled, mergeProfiles } = this.props;
-
     const swapButtonClasses = classNames('btn-floating', 'blue', {
       'waves-effect': controlsEnabled,
       'waves-light': controlsEnabled,
       'disabled': !controlsEnabled
     });
-
     const selectedMergeProfile = mergeProfiles.find(({key}) => key === this.props.selectedMergeProfile);
-
-
     return (
       <div className="row row-margin-swap record-selection-controls">
-
         <div className="col s2 offset-s1 input-field">
-          <input id="source_record" type="tel" value={this.props.sourceRecordId} onChange={this.handleChange.bind(this)} disabled={!controlsEnabled} />
+          <input 
+            id="source_record" 
+            type="tel" 
+            value={this.props.sourceRecordId} 
+            onChange={this.handleChange.bind(this)} 
+            disabled={!controlsEnabled} />
           <label htmlFor="source_record">Lähdetietue</label>
         </div>
-
         <div className="col s2 control-swap-horizontal input-field">
           <div>
-            <a className={swapButtonClasses} onClick={(e) => this.handleSwap(e)}>
-              <i className="material-icons tooltip small" title="Vaihda keskenään">swap_horiz</i>
+            <a 
+              className={swapButtonClasses} 
+              onClick={(e) => this.handleSwap(e)}>
+              <i 
+                className="material-icons tooltip small" 
+                title="Vaihda keskenään">swap_horiz
+              </i>
             </a>
           </div>
         </div>
-
-        <div className="col s2 input-field">
-          <input id="target_record" type="tel" value={this.props.targetRecordId} onChange={this.handleChange.bind(this)} disabled={!controlsEnabled}/>
+        <div 
+          className="col s2 input-field">
+          <input 
+            id="target_record" 
+            type="tel" 
+            value={this.props.targetRecordId} 
+            onChange={this.handleChange.bind(this)} 
+            disabled={!controlsEnabled}/>
           <label htmlFor="target_record">Pohjatietue</label>
         </div>
-
         <div className="col s2 offset-s2 profile-selector input-field">
           {mergeProfiles.length > 1 && (
             <div className="input-field">
-              <select defaultValue={selectedMergeProfile.key} ref={(ref) => this.mergeProfileSelect = ref}>
+              <select 
+                defaultValue={selectedMergeProfile.key} 
+                ref={(ref) => this.mergeProfileSelect = ref}>
                 {mergeProfiles.map(({key, name}) => (
                   <option key={key} value={key}>{name}</option>
                 ))}
@@ -215,22 +214,28 @@ export class RecordSelectionControls extends React.Component {
             </div>
           )}
          {selectedMergeProfile.description && (
-            <a href="#" data-activates="profile-selector-info" onClick={(e) => this.displayProfileInfo(e)}>
-              <i className="material-icons" title="Kuvaus">info</i>
+            <a 
+              href="#" 
+              data-activates="profile-selector-info" 
+              onClick={(e) => this.displayProfileInfo(e)}>
+              <i 
+                className="material-icons" 
+                title="Kuvaus">info
+              </i>
             </a>
           )}
-
           {selectedMergeProfile.description && this.state.displayProfileInfo && (
-            <div id="profile-selector-info" className="card" ref={(ref) => this.profileInfoDialog = ref}>
+            <div 
+              id="profile-selector-info" 
+              className="card" 
+              ref={(ref) => this.profileInfoDialog = ref}>
               <div className="card-content">{selectedMergeProfile.description}</div>
             </div>
           )}
-
         </div>
       </div>
     );
   }
-
 }
 
 function mapStateToProps(state) {
