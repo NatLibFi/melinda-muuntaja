7d5729ea217d09ae73003f90d580107068916865
Harri Huhtala <harri.huhtala@mystes.fi>
2018-12-17 13:05:24 +0200

    removed 588 import

--- frontend/js/config/e-to-print/postmerge/eToPrint-postmerge.js
+++ frontend/js/config/e-to-print/postmerge/eToPrint-postmerge.js
@@ -48,7 +48,7 @@ function addTag(mergedRecordParam, tag) {
 
 // eToPrint postmerge functions ->
 export function replaceFieldsFromSource(targetRecord, sourcerecord, mergedRecordParam) {
-  const mergeConfigurationFields = /^(1..|041|080|084|240|245|246|250|260|263|264|490|500|502|505|509|520|567|588|6[^5].|65[^5]|700|710|711|800|810|811|830)$/;
+  const mergeConfigurationFields = /^(1..|041|080|084|240|245|246|250|260|263|264|490|500|502|504|505|509|520|546|567|6[^5].|65[^5]|700|710|711|800|810|811|830)$/;
 
   const fieldsFromSourceRecord = sourcerecord.fields.filter(field => mergeConfigurationFields.test(field.tag));
   
